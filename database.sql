f4a20b3d5e840679fff1af6b800cc41546328a3a
abhidupate2203 <abhi.dupate@gmail.com>
2016-12-21 23:38:55 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `malala`.`posts` ( `post_id` INT(10) NOT NULL AUTO_INCREMENT , `post_title` VARCHAR(100) NOT NULL , `post_date` DATE NOT NULL , `post_author` VARCHAR(100) NOT NULL , `post_image` TEXT NOT NULL , `post_keywords` TEXT NOT NULL , `post_content` TEXT NOT NULL , PRIMARY KEY (`post_id`)) ENGINE = InnoDB;
